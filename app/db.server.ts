c16cb3ae1917f93e4d6b3f528485016cc4dcd079
khushwantsabai <khushwantsabai@gmail.com>
2026-06-23 16:48:38 +0530

    Migrate Prisma to Neon Serverless Driver

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,15 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 declare global {
-  var prismaGlobal: PrismaClient;
+  var prismaGlobal: PrismaClient | undefined;
 }
+
+const connectionString = process.env.DATABASE_URL || "";
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient({ adapter });
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma = global.prismaGlobal ?? new PrismaClient({ adapter });
 
 export default prisma;
